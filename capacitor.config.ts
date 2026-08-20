de717a7d698d8e4225f8cce1e02a53f573ea4979
Brandy Smith <6577830+brandyscarney@users.noreply.github.com>
2026-08-20 15:45:54 -0400

    chore: sync capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,6 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.demo.pg.vue',
   appName: 'Vue Photo Gallery',
-  bundledWebRuntime: false,
   webDir: 'dist',
   plugins: {
     SplashScreen: {
